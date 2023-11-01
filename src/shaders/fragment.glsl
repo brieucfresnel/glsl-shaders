386a758f759e5742ab4f07d6caa6478c01ff4337
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-01 23:20:32 +0100

    create a drawCicle function

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -3,7 +3,14 @@ uniform float uRadius;
 varying vec3 vPosition;
 varying vec3 vNormal;
 varying vec2 vUv;
+// vec3(step(uRadius, length(vUv - 0.5))), 1
+
+float drawCircle(vec2 position, vec2 center) {
+	return distance(position, center);
+}
+
+const vec2 center = vec2(0.5);
 
 void main() {
-	gl_FragColor = vec4(vec3(step(0.99, 1.0 - abs(vUv.x - 0.5))), 1);
+	gl_FragColor = vec4(vec3(drawCircle(vUv, center)), 1);
 }
