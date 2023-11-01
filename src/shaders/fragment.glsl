aefdee5c8124a48220335412026f9a925b74275a
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2023-11-01 22:19:35 +0100

    make a sphere using sstep andd length functions

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -5,6 +5,9 @@ varying vec2 vUv;
 void main() {
 
 	vec2 uv = vUv;
+	uv -= vec2(0.5);
+	uv *= 2.0;
 
-	gl_FragColor = vec4(vec3(length(uv)), 1.0);
+	const float RADIUS = 0.8;
+	gl_FragColor = vec4(vec3(step(RADIUS, length(uv))), 1);
 }
